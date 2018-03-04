092b0bd0aec3c44003d7d7706a4ada5e5eee9c41
TBSjiga <31573154+TBSjiga@users.noreply.github.com>
2018-03-04 22:27:58 +0300

    Update complex.cpp

--- complex.cpp
+++ complex.cpp
@@ -43,35 +43,35 @@ void printresult(double z1, double z2)
 		else cout << endl;
 	}
 }
-complex add(complex a, complex b)
+void add(complex *a, complex *b)
 {
 	double z1, z2;
-	z1 = a.Re + b.Re;
-	z2 = a.Im + b.Im;
+	z1 = a->Re + b->Re;
+	z2 = a->Im + b->Im;
 	printresult(z1, z2);
-	return a;
+	return;
 }
-complex multiply(complex a, complex b)
+void multiply(complex *a, complex *b)
 {
 	double z1, z2;
-	z1 = a.Re * b.Re - a.Im * b.Im;
-	z2 = a.Re * b.Im + b.Re * a.Im;
+	z1 = a->Re * b->Re - a->Im * b->Im;
+	z2 = a->Re * b->Im + b->Re * a->Im;
 	printresult(z1, z2);
-	return a;
+	return;
 }
-complex substract(complex a, complex b)
+void substract(complex *a, complex *b)
 {
 	double z1, z2;
-	z1 = a.Re - b.Re;
-	z2 = a.Im - b.Im;
+	z1 = a->Re - b->Re;
+	z2 = a->Im - b->Im;
 	printresult(z1, z2);
-	return a;
+	return;
 }
-complex devide(complex a, complex b)
+void devide(complex *a, complex *b)
 {
 	double z1, z2;
-	z1 = (a.Re * b.Re + a.Im * b.Im) / (b.Re * b.Re + b.Im * b.Im);
-	z2 = (b.Re * a.Im - a.Re * b.Im) / (b.Re * b.Re + b.Im * b.Im);
+	z1 = (a->Re * b->Re + a->Im * b->Im) / (b->Re * b->Re + b->Im * b->Im);
+	z2 = (b->Re * a->Im - a->Re * b->Im) / (b->Re * b->Re + b->Im * b->Im);
 	printresult(z1, z2);
-	return a;
+	return;
 }
