7602b1116f505dc5ce332175479d62ac32d76292
TBSjiga <31573154+TBSjiga@users.noreply.github.com>
2018-03-06 14:12:46 +0300

    Update complex.h

--- complex.h
+++ complex.h
@@ -2,16 +2,23 @@
 #define CALCULATOR_H 
 #include <iostream>
 #include <math.h>
+
 struct complex
 {
 	double Re;
 	double Im;
 };
-typedef struct complex * Complex;
+typedef struct complex *Complex;
+
 void printresult(double z1, double z2);
-void add(complex *a, complex *b);
-void multiply(complex *a, complex *b);
-void substract(complex *a, complex *b);
-void devide(complex *a, complex *b);
+void menu(Complex a, Complex b);
+void menu_1(double *Re, double *Im);
+void menu_2(double *Re, double *Im);
+
+Complex complexCreate(double *Re, double *Im);
+Complex add(Complex a, Complex b);
+Complex multiply(Complex a, Complex b);
+Complex substract(Complex a, Complex b);
+Complex devide(Complex a, Complex b);
 
 #endif CALCULATOR_H
