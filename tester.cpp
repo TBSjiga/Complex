d0c4a755545246642bd783d61d638b6e40b9223d
TBSjiga <31573154+TBSjiga@users.noreply.github.com>
2018-03-06 14:15:17 +0300

    Update tester.cpp

--- tester.cpp
+++ tester.cpp
@@ -1,66 +1,18 @@
 #include "calculator.h" 
 #include <iostream> 
 using namespace std;
-void add(complex *a, complex *b);
-void multiply(complex *a, complex *b);
-void substract(complex *a, complex *b);
-void devide(complex *a, complex *b);
-
 int main()
 {
 	while (1)
 	{
-		complex a, b;
-		char op, o;
-		cout << "_____________________________________________" << endl;
-		cout << "Enter Re and Im of the first complex:" << endl;
-		cin >> a.Re >> a.Im;
-		cout << endl;
-		cout << "Enter Re and Im of the second complex:" << endl;
-		cin >> b.Re >> b.Im;
-		cout << endl;
-		cout << "Chose operation:" << endl;
-		cout << "+   -   Addition" << endl;
-		cout << "-   -   Substract" << endl;
-		cout << "*   -   Multiply" << endl;
-		cout << "/   -   Devide" << endl;
-		cout << endl;
-		cin >> op;
-		switch (op)
-		{
-		case '+':
-		{
-			add(&a, &b);
-		}
-		break;
-		case '-':
-		{
-			substract(&a, &b);
-		}
-		break;
-		case '*':
-		{
-			multiply(&a, &b);
-		}
-		break;
-		case '/':
-		{
-			devide(&a, &b);
-		}
-		break;
-		default:
-		{
-			cout << "Please, chose correct operation" << endl;
-		}
-		return 0;
-		}
-		cout << "_____________________________________________" << endl;
-		cout << endl;
-		cout << "Press any button to continue. Press N to exit." << endl;
-		cin >> o;
-		if (o == 'N' || o == 'n')
-		{
-			exit(0);
-		}
+		double Re = 0, Im = 0;
+
+		menu_1(&Re, &Im);
+		Complex a = complexCreate(&Re, &Im);
+
+		menu_2(&Re, &Im);
+		Complex b = complexCreate(&Re, &Im);
+
+		menu(a,b);
 	}
 }
