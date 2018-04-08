10e37f14e4932f4dfc1431b3efe99de4ecc088c4
TBSjiga <31573154+TBSjiga@users.noreply.github.com>
2018-04-09 01:33:28 +0300

    Add files via upload

--- complex.h
+++ complex.h
@@ -4,24 +4,6 @@
 #include <math.h>
 using namespace std;
 
-/*struct complex
-{
-	double Re;
-	double Im;
-};
-typedef struct complex *Complex;
-
-void printresult(double z1, double z2);
-void menu(Complex a, Complex b);
-void menu_1(double *Re, double *Im);
-void menu_2(double *Re, double *Im);
-
-Complex complexCreate(double *Re, double *Im);
-Complex add(Complex a, Complex b);
-Complex multiply(Complex a, Complex b);
-Complex substract(Complex a, Complex b);
-Complex devide(Complex a, Complex b);*/
-
 class Complex
 {
 public:
